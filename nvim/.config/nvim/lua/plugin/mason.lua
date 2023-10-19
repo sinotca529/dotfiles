1a5dabed6a21c6ce4e5cf2a78f6c5e79ca27d48a
sinotca529 <52117877+sinotca529@users.noreply.github.com>
2023-10-19 21:03:32 +0900

    nvim : change mason ui

--- nvim/.config/nvim/lua/plugin/mason.lua
+++ nvim/.config/nvim/lua/plugin/mason.lua
@@ -2,6 +2,10 @@ return {
     'williamboman/mason.nvim',
     cmd = 'Mason',
     config = function()
-        require('mason').setup({})
+        require('mason').setup({
+            ui = {
+                border = 'single'
+            }
+        })
     end,
 }
