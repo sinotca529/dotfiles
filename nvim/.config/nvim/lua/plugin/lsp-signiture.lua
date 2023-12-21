2bd516fcc733d887c506b618e766b7e9d3044eea
sinotca529 <52117877+sinotca529@users.noreply.github.com>
2023-12-21 21:33:21 +0900

    nvim : change lsp_signiture's hint icon

--- nvim/.config/nvim/lua/plugin/lsp-signiture.lua
+++ nvim/.config/nvim/lua/plugin/lsp-signiture.lua
@@ -2,6 +2,8 @@ return {
     'ray-x/lsp_signature.nvim',
     event = 'LspAttach',
     config = function()
-        require('lsp_signature').on_attach()
+        require('lsp_signature').on_attach({
+            hint_prefix = " "
+        })
     end,
 }
