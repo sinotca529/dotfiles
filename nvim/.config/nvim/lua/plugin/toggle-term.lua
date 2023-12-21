cae7f8be058524fc32432c1f770e5930f43ca2ce
sinotca529 <52117877+sinotca529@users.noreply.github.com>
2023-12-21 21:33:42 +0900

    nvim : widen floatterm

--- nvim/.config/nvim/lua/plugin/toggle-term.lua
+++ nvim/.config/nvim/lua/plugin/toggle-term.lua
@@ -11,6 +11,11 @@ return {
             insert_mappings = true,
             close_on_exit = true,
             direction = 'float',
+            float_opts = {
+                border = 'curved',
+                width = function() return math.floor(vim.o.columns * 0.9) end,
+                height = function() return math.floor(vim.o.lines * 0.9) end,
+            },
         })
     end
 }
