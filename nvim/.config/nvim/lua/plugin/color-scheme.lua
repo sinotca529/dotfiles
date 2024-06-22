d24371c7f9dc663179a9f9006f3027850ea47e7f
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2024-06-22 14:45:52 +0900

    remove unused colorschemes

--- nvim/.config/nvim/lua/plugin/color-scheme.lua
+++ nvim/.config/nvim/lua/plugin/color-scheme.lua
@@ -1,22 +1,6 @@
 return {
-    'sainnhe/gruvbox-material',
-    dependencies = {
-        'edeneast/nightfox.nvim',
-        'glepnir/zephyr-nvim',
-        'vigoux/oak',
-        'catppuccin/nvim',
-    },
+    'glepnir/zephyr-nvim',
     config = function()
-        vim.cmd.colorscheme('gruvbox-material')
-        vim.cmd('hi Comment cterm=NONE')
-        vim.cmd('hi Comment gui=NONE')
-        vim.cmd('hi Normal ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi NormalFloat ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi FloatBorder ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi NormalNC ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi NonText ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi LineNr ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi Folded ctermbg=NONE guibg=#NONE')
-        vim.cmd('hi EndOfBuffer ctermbg=NONE guibg=#NONE')
+        vim.cmd.colorscheme('zephyr')
     end
 }
