04709b0f353c064b8cfcfb5700b47783716573ee
Takumi Watanabe <52117877+sinotca529@users.noreply.github.com>
2024-12-22 15:53:28 +0900

    nvim: change hop keybind

--- nvim/.config/nvim/lua/plugin/hop.lua
+++ nvim/.config/nvim/lua/plugin/hop.lua
@@ -7,9 +7,9 @@ return {
 
         local d = require('hop.hint').HintDirection
         local o = { remap = true, silent = true, }
-        vim.keymap.set('n', 'f', function() hop.hint_char1({ direction = d.AFTER_CURSOR }) end, o)
-        vim.keymap.set('n', 'F', function() hop.hint_char1({ direction = d.BEFORE_CURSOR }) end, o)
-        vim.keymap.set('n', 't', function() hop.hint_char1({ direction = d.AFTER_CURSOR, hint_offset = -1 }) end, o)
-        vim.keymap.set('n', 'T', function() hop.hint_char1({ direction = d.BEFORE_CURSOR, hint_offset = 1 }) end, o)
+        vim.keymap.set('n', '<leader><leader>f', function() hop.hint_char1({ direction = d.AFTER_CURSOR }) end, o)
+        vim.keymap.set('n', '<leader><leader>F', function() hop.hint_char1({ direction = d.BEFORE_CURSOR }) end, o)
+        vim.keymap.set('n', '<leader><leader>t', function() hop.hint_char1({ direction = d.AFTER_CURSOR, hint_offset = -1 }) end, o)
+        vim.keymap.set('n', '<leader><leader>T', function() hop.hint_char1({ direction = d.BEFORE_CURSOR, hint_offset = 1 }) end, o)
     end
 }
